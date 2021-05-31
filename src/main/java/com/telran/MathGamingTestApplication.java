a09f1d45cf2805a24eb5139c6b6913a2d7d567a3
Alla Chybisova <allachybisova@gmail.com>
2021-05-31 10:22:07 +0300

    init repo

---
+++ src/main/java/com/telran/MathGamingTestApplication.java
@@ -0,0 +1,13 @@
+package com.telran;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MathGamingTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MathGamingTestApplication.class, args);
+    }
+
+}
